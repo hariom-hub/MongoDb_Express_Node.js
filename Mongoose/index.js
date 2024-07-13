d8369bcd2290680e1a855b6829628f4f3dc7acc8
Hariom Singh Thakur <hariomthakur638@gmail.com>
2024-07-14 00:30:34 +0530

    I am grinding....

--- Mongoose/index.js
+++ Mongoose/index.js
@@ -1,3 +1,96 @@
+
 const mongoose = require("mongoose");
-//connecting with mongodb database
-mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+main().then(() => {
+
+    console.log("connection successful");
+}).catch((error) => {
+
+    console.log(error);
+})
+
+async function main() {
+
+    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+}
+
+
+//defining the schema
+
+const Userschema = new mongoose.Schema({
+
+    name: String,
+    age: Number,
+    degree: String,
+    rollNo: Number
+});
+
+const Userdata = new mongoose.Schema({
+
+    name: String,
+    height: Number,
+    aadharno: Number,
+    gender: String
+});
+
+
+const laptop = mongoose.model("laptop", Userschema);
+const mobile = mongoose.model("mobile", Userdata);
+const user = mongoose.model("user", Userschema);
+
+//inserting into user(inside the class Model by creating it's object (instance))
+
+const uesr1 = new user({
+
+    name: "hariom singh thakur",
+    age: 20,
+    degree: "b.tech",
+    rollNo: 2404488
+});
+
+
+const user2 = new user({
+
+    name: "gaurisha singh thakur",
+    age: 19,
+    degree: "bca",
+    rollNo: 240438
+});
+
+const user3 = new user({
+
+    name: "shreesha singh thakur",
+    age: 23,
+    degree: "gds",
+    rollNo: 2303354
+});
+
+// user2.save().then((result) => {
+
+//     console.log(result);
+// }).catch((error) => {
+
+//     console.log(error);
+// })
+
+// user3.save().then((result) => {
+
+//     console.log(result);
+// }).catch((error) => {
+
+//     console.log(error);
+// });
+
+user.insertMany(
+    [
+    { name: "sundar pichai", age: 54, degree: "b.tech,ms,mba" },
+    { name: "satya nadella", age: 56, degree: "b.tech,m.tech,mba" },
+    { name: "arjuna", age: 4000, degree: "vedas,upanishads", rollNo: 1 },
+    { name: "aacharya prashant", age: 45, degree: "b.tech,mba", rollNo: 1.1 },
+    ]
+).then((result) => {
+    console.log(result);
+}).catch((error) => {
+
+    console.log(error);
+})
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # MongoDb_Express_Node.js
-let's integrate backend with mongodb
+let's integrate backend with mongod.
+
+# Schema
+
+schema defines the shape of the documents within that collection.
+const userSchema = new mongoose.Schema({
+
+    name:String,
+    age:Number,
+    degree:String
+})
+
+# Model
+
+model in mongoose is a class with which we construct documents.
+
+const User = mongoose.model("user",userSchema);
+
+# Insert 
